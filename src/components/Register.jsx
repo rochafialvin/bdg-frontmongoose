690b1aa4ff24bf9dbd3d68725122b5a750f674dd
rochafialvin <rochafi.dev@gmail.com>
2019-09-26 14:14:42 +0700

    ref

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,6 +2,19 @@ import React, { Component } from 'react'
 
 class Register extends Component {
 
+
+    onSubmitClick = () => {
+        // Ambil data dari text box
+        let username = this.username.value
+        let email = this.email.value
+        let password = this.sandi.value
+
+        console.log(username, email, password)
+
+        // Simpan data di json
+
+    }
+
     render() {
         return (
             <div className='col-6 mx-auto mt-5 card'>
@@ -13,18 +26,19 @@ class Register extends Component {
                         <div className='card-title'>
                             <h4>Username</h4>
                         </div>
-                        <input className='form-control' type='text'/>
+                        <input ref={ (asdf) => {this.username = asdf} } className='form-control' type='text'/>
 
                         <div className='card-title'>
                             <h4>Email</h4>
                         </div>
-                        <input className='form-control' type='email'/>
+                        <input ref={ (input) => {this.email = input} } className='form-control' type='email'/>
 
                         <div className='card-title'>
                             <h4>Password</h4>
                         </div>
-                        <input className='form-control' type='password'/>
+                        <input ref={ (input) => {this.sandi = input} } className='form-control' type='password'/>
                     </form>
+                    <button onClick={this.onSubmitClick} className='btn btn-outline-primary btn-block'>Submit</button>
                 </div>
             </div>
         )
@@ -32,3 +46,7 @@ class Register extends Component {
 }
 
 export default Register
+
+// Untuk memberikan referensi ke suatu tag input, kita menggunakan attribute 'ref'
+    // akan menerima function, function yg di terima akan memiliki satu inputan
+    // nama inputan tersebut bebas, biasanya akan di beri nama 'input'
