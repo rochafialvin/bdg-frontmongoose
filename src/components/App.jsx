88c3b0f3d93ec671ca99ce0a699a126b7dbc6906
rochafialvin <rochafi.dev@gmail.com>
2019-09-26 11:02:39 +0700

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class App extends Component{
+
+    render() {
+        return (
+            <div>
+                <h3>App Component</h3>
+            </div>
+        )
+    }
+}
+
+export default App
