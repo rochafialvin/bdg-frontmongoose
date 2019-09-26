c6bc93094a812a5c6b58c8085665405684fa4a4a
rochafialvin <rochafi.dev@gmail.com>
2019-09-26 11:26:26 +0700

    Slash Login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,19 @@
 import React, {Component} from 'react'
+import {BrowserRouter , Route} from 'react-router-dom'
+
+import Home from './Home'
+import Register from './Register'
+import Login from './Login'
 
 class App extends Component{
 
     render() {
         return (
-            <div>
-                <h3>App Component</h3>
-            </div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/login" component={Login} />
+                </div>
+            </BrowserRouter>
         )
     }
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,8 +4,23 @@ class Login extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Login Component</h1>
+            <div className='card'>
+                <div className='card-body'>
+                    <div className='border-bottom border-secondary card-title'>
+                        <h1>Login</h1>
+                    </div>
+                    <form >
+                        <div>
+                            <h4>Username</h4>
+                        </div>
+                        <input type='text'/>
+
+                        <div>
+                            <h4>Password</h4>
+                        </div>
+                        <input type='password'/>
+                    </form>
+                </div>
             </div>
         )
     }
