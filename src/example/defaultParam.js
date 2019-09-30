3c3c51a6d0436f41829a3af235681889ed758b78
rochafialvin <rochafi.dev@gmail.com>
2019-09-30 11:31:19 +0700

    create reducer

---
+++ src/example/defaultParam.js
@@ -0,0 +1,12 @@
+
+let jumlah = (angka1, angka2 = 3) => {
+
+    return angka1 + angka2
+
+}
+
+//  4 + 5
+jumlah(4, 5)
+
+//  4 + 3
+jumlah(4)
---
+++ src/reducers/index.js
@@ -0,0 +1,34 @@
+import { combineReducers } from 'redux'
+
+let initState = {
+    id: 0,
+    username: ""
+}
+
+let authReducer = (state = initState, action) => {
+    switch (action.type) {
+        case "LOGIN_SUCCESS":
+            // do something
+            break;
+        
+        case "LOGOUT_SUCCES":
+            // do something
+    
+        default:
+            return state
+    }
+}
+
+combineReducers(
+    {
+        auth: authReducer
+    }
+)
+
+// Action adalah object yang memiliki dua properties
+    // type: untuk menentukan reducer mana yang akan mengolah
+    //  payload: berisi data yang dibuthkan untuk di olah
+    // {
+    //     type: "LOGIN_SUCCESS",
+    //     payload: { id: 32 , username: "Steve" }
+    // }
