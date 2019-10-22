44ed7b5e07146f6a499169af4b5a3feda214ab59
rochafialvin <rochafi.dev@gmail.com>
2019-10-22 11:01:02 +0700

    Handling error register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,99 +1,36 @@
 import React, { Component } from 'react'
-
-import axios from 'axios'
+import axios from '../config/axios'
 
 class Register extends Component {
-
-
 
     onSubmitClick = () => {
         // Ambil data dari text box
         let _username = this.username.value // rochafi
+        let _name = this.name.value
+        let _age = this.age.value
         let _email = this.email.value // rochafi@gmail.com
         let _password = this.sandi.value
 
-        axios.get('http://localhost:2099/users')
-            .then((res) => {
-                /*
-                    res.data = [
-                        {username: alex , email: alex19@gmail.com},
-                        {username: rochafi, email: rochafi@gmail.com},
-                        {username: rexy , email: rexytofa@gmail.com}
-                    ]
-                */
-                // user = {id, username, email, password}
-                let takenUsername = res.data.filter((user) => {
-                    return user.username === _username // rochafi === rochafi
-                })
-                /*
-                    [
-                        {username: rochafi, email: rochafi@gmail.com}
-                    ]
-                */
+        // Register user
+        axios.post(
+            '/users',
+            {
+                username: _username,
+                name: _name,
+                age : _age,
+                email: _email,
+                password: _password
+            }
+        ).then(res => {
+            if(res.data.error){
+                return alert(res.data.error)
+            }
 
+            alert('Registrasi berhasil')
 
-                if(takenUsername.length > 0){
-                    return console.log("Username has been taken")
-                }
-
-                let takenEmail = res.data.filter((user) => {
-                    return user.email === _email // rochafi === rochafi
-                })
-
-                if(takenEmail.length > 0){
-                    return console.log("Email has been taken")
-                }
-
-                axios.post(
-                    'http://localhost:2099/users',
-                    {
-                        username: _username,
-                        email: _email,
-                        password: _password
-                    }
-                ).then(() => {
-                    alert("Berhasil terdaftar")
-                })
-
-                
-            })
-
-        // axios.get('http://localhost:2099/users', {params:{ username:_username }})
-        //     .then((res) => {
-        //         if(res.data.length > 0){
-        //             alert("Username has been taken")
-        //         } else {
-        //             axios.get('http://localhost:2099/users', {params: {email: _email}})
-        //                 .then((res) => {
-        //                     if(res.data.length > 0){
-        //                         alert("Email has been taken")
-        //                     } else {
-                                // axios.post(
-                                //     'http://localhost:2099/users',
-                                //     {
-                                //         username: _username,
-                                //         email: _email,
-                                //         password: _password
-                                //     }
-                                // ).then(() => {
-                                //     alert("Berhasil terdaftar")
-        //                         })
-        //                     }
-        //                 })
-        //         }
-
-        //     })
-
-
-        // POST data
-        // axios.post(
-        //     'http://localhost:2099/users', 
-        //     {
-        //         username: _username,
-        //         email: _email,
-        //         password: _password
-        //     }
-        // )
+        }).catch(err => {
+            console.log({err})
+        })
 
     }
 
@@ -108,7 +45,17 @@ class Register extends Component {
                         <div className='card-title'>
                             <h4>Username</h4>
                         </div>
-                        <input ref={ (asdf) => {this.username = asdf} } className='form-control' type='text'/>
+                        <input ref={ (input) => {this.username = input} } className='form-control' type='text'/>
+                        
+                        <div className='card-title'>
+                            <h4>Name</h4>
+                        </div>
+                        <input ref={ (input) => {this.name = input} } className='form-control' type='text'/>
+                        
+                        <div className='card-title'>
+                            <h4>Age</h4>
+                        </div>
+                        <input ref={ (input) => {this.age = input} } className='form-control' type='text'/>
 
                         <div className='card-title'>
                             <h4>Email</h4>
